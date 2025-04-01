0f834d6a841d1d523916728412b164fbd7582a95
SneaX972 <cantanicolas@gmail.com>
2025-04-01 14:03:33 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,9 +4,9 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
@@ -23,13 +23,24 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
+
+
+
+
 if __name__ == "__main__":
-  app.run(debug=True)
-  
+    app.run(debug=True)
